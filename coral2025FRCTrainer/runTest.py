b7cdcd1f1d3dc3534b3ce9c90657266c182da9a7
Dark-303 <156324532+Dark-303@users.noreply.github.com>
2025-11-19 16:16:43 -0500

    Set up for ver 3

--- coral2025FRCTrainer/runTest.py
+++ coral2025FRCTrainer/runTest.py
@@ -5,11 +5,18 @@ Current Models:
 - coral_modelVER1: 
     Path: runs/train/coral_modelVER1/weights/best.pt
     Epochs Trained: 50
+    Changes: N/A
     Grade: Satisfactory for initial testing
 - coral_modelVER2:
     Path: runs/train/coral_modelVER2/weights/best.pt
     Epochs Trained: 100
+    Changes: Increased epochs
     Grade: Good performance but can be improved in most areas
+- coral_modelVER3:
+    Path: runs/train/coral_modelVER3/weights/best.pt
+    Epochs Trained: 200
+    Changes: Evolved VER2 with more epochs, increased image size to 612
+    Grade: TBD - Training
 '''
 model = YOLO("runs/train/coral_modelVER2/weights/best.pt")
 
--- coral2025FRCTrainer/trainer.py
+++ coral2025FRCTrainer/trainer.py
@@ -1,15 +1,18 @@
 from ultralytics import YOLO
 
-# Load model (pretrained YOLOv5n)
-model = YOLO("yolov5nu.pt")  # or yolov5nu.onnx if using ONNX as starting point
+# Load default YOLOv5n model
+#model = YOLO("yolov5nu.pt")  # or yolov5nu.onnx if using ONNX as starting point
+
+# Evolve model from an existing trained model
+model = YOLO("runs/train/coral_modelVER2/weights/best.pt")
 
 # Train the model
 model.train(
     data="dataset/data.yaml",  # your data.yaml path
-    imgsz=416,                 # input image size
+    imgsz=612,                 # input image size
     epochs=100,                 # adjust for your needs
     batch=4,                  # adjust depending on your GPU/CPU
     project="runs/train",      # folder to save results
-    name="coral_modelVER2",   # subfolder name
+    name="coral_modelVER3",   # subfolder name
     exist_ok=True
 )
