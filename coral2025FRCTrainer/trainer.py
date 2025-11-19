ecb8acd31ba8fbdb9bdab382e488f0f8497f9a17
Dark-303 <156324532+Dark-303@users.noreply.github.com>
2025-11-19 13:17:48 -0500

    preparing for training session, adding val

--- coral2025FRCTrainer/trainer.py
+++ coral2025FRCTrainer/trainer.py
@@ -6,9 +6,9 @@ model = YOLO("yolov5nu.pt")  # or yolov5nu.onnx if using ONNX as starting point
 # Train the model
 model.train(
     data="dataset/data.yaml",  # your data.yaml path
-    imgsz=640,                 # input image size
+    imgsz=416,                 # input image size
     epochs=50,                 # adjust for your needs
-    batch=16,                  # adjust depending on your GPU/CPU
+    batch=4,                  # adjust depending on your GPU/CPU
     project="runs/train",      # folder to save results
     name="coral_modelVER1",   # subfolder name
     exist_ok=True
