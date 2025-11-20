5cc9d0a54c34f611c39e828152d80e38e6d332c8
Dark-303 <156324532+Dark-303@users.noreply.github.com>
2025-11-19 19:57:26 -0500

    do some file reorganizing. Updated versions with 0. prefix. Ready to train 1.1

--- coral2025FRCTrainer/runTest.py
+++ coral2025FRCTrainer/runTest.py
@@ -2,28 +2,28 @@ from ultralytics import YOLO
 
 '''
 Current Models:
-- coral_modelVER1: 
-    Path: runs/train/coral_modelVER1/weights/best.pt
+- coral_modelVER0.1: 
+    Path: runs/train/coral_modelVER0/coral_modelVER1/weights/best.pt
     Epochs Trained: 50
     Changes: N/A
     Grade: Satisfactory for initial testing
-- coral_modelVER2:
-    Path: runs/train/coral_modelVER2/weights/best.pt
+- coral_modelVER0.2:
+    Path: runs/train/coral_modelVER0/coral_modelVER2/weights/best.pt
     Epochs Trained: 100
     Changes: Increased epochs
     Grade: Good performance but can be improved in most areas
-- coral_modelVER3:
-    Path: runs/train/coral_modelVER3/weights/best.pt
+- coral_modelVER0.3:
+    Path: versions/coral_modelVER0.3.pt
     Epochs Trained: 200
     Changes: Evolved VER2 with more epochs, increased image size to 612
-    Grade: TBD - Training
+    Grade: Fairly accurate, cannot detect verticals and certain angles
 '''
-model = YOLO("runs/train/coral_modelVER3/weights/best.pt")
+model = YOLO("versions/coral_modelVER0.3.pt")
 
 results = model(
     "dataset/images/train",
     save=True,
-    project="coral_modelVER3_detect",  # your custom folder
+    project="tests/coral_modelVER0/coral_modelVER0.3_detect",  # your custom folder
     name="predict02",                    # subfolder name
     exist_ok=True                      # prevents YOLO from adding numbers
 )
--- coral2025FRCTrainer/trainer.py
+++ coral2025FRCTrainer/trainer.py
@@ -4,15 +4,15 @@ from ultralytics import YOLO
 #model = YOLO("yolov5nu.pt")  # or yolov5nu.onnx if using ONNX as starting point
 
 # Evolve model from an existing trained model
-model = YOLO("runs/train/coral_modelVER2/weights/best.pt")
+model = YOLO("yolsov5n.pt")
 
 # Train the model
 model.train(
     data="dataset/data.yaml",  # your data.yaml path
     imgsz=612,                 # input image size
-    epochs=100,                 # adjust for your needs
+    epochs=300,                 # adjust for your needs
     batch=4,                  # adjust depending on your GPU/CPU
-    project="runs/train",      # folder to save results
-    name="coral_modelVER3",   # subfolder name
+    project="runs/train/coral_modelVER1",      # folder to save results
+    name="coral_modelVER1.1",   # subfolder name
     exist_ok=True
 )
